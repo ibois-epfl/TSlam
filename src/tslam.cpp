d9779765a02b3f070c527ff9e14e88b2d98f5f1d
Petingo <eyanghongbin0315@gmail.com>
2022-08-25 09:45:14 +0200

    Modify system update schedule

--- src/tslam.cpp
+++ src/tslam.cpp
@@ -2,129 +2,146 @@
 #include "utils/system.h"
 #include "basictypes/debug.h"
 namespace tslam{
-TSlam::TSlam(){
-    impl=new System;
-}
-TSlam::~TSlam(){
-    delete (System*)impl;
-}
-void TSlam::setParams(std::shared_ptr<Map> map, const  tslam::Params &params, const std::string &vocabulary){
-    reinterpret_cast<System*>(impl)->setParams(map,params,vocabulary);
-}
-void TSlam::clear(){
-    reinterpret_cast<System*>(impl)->clear();
-}
-Params  & TSlam::getParams() {
-    return System::getParams();
-}
-cv::Mat TSlam::process(  cv::Mat &in_image,const ImageParams &ip,uint32_t frameseq_idx){
-    if(in_image.size()!=ip.CamSize)
-        throw  std::runtime_error("Input Image Size is Different from Image Params");
-    return  reinterpret_cast<System*>(impl)->process(in_image,ip,frameseq_idx);
-}
-cv::Mat TSlam::processStereo( cv::Mat &in_image,const cv::Mat &R_image,const ImageParams &ip,uint32_t frameseq_idx){
-    return  reinterpret_cast<System*>(impl)->process(in_image,ip,frameseq_idx,cv::Mat(),R_image);
-}
-cv::Mat TSlam::processRGBD( cv::Mat &in_image,const cv::Mat & depth,const ImageParams &ip,uint32_t frameseq_idx){
-    return  reinterpret_cast<System*>(impl)->process(in_image,ip,frameseq_idx,depth);
-}
-cv::Mat TSlam::processArray(vector<cv::Mat> &images,  ImageParams &ArrayCamParams ,uint32_t frameseq_idx)
-{
-    return  reinterpret_cast<System*>(impl)->process(images, ArrayCamParams, frameseq_idx);
-}
-void TSlam::resetTracker(){
-    reinterpret_cast<System*>(impl)->resetTracker();
-}
-void TSlam::setMode(MODES mode){
-    reinterpret_cast<System*>(impl)->setMode(mode);
-}
+    TSlam::TSlam(){
+        impl=new System;
+    }
+    TSlam::~TSlam(){
+        delete (System*)impl;
+    }
+    void TSlam::setParams(std::shared_ptr<Map> map, const  tslam::Params &params, const std::string &vocabulary){
+        reinterpret_cast<System*>(impl)->setParams(map,params,vocabulary);
+    }
+    void TSlam::clear(){
+        reinterpret_cast<System*>(impl)->clear();
+    }
+    Params  & TSlam::getParams() {
+        return System::getParams();
+    }
+    cv::Mat TSlam::process(  cv::Mat &in_image,const ImageParams &ip,uint32_t frameseq_idx){
+        updateSystem();
+        if(in_image.size()!=ip.CamSize)
+            throw  std::runtime_error("Input Image Size is Different from Image Params");
+        return  reinterpret_cast<System*>(impl)->process(in_image,ip,frameseq_idx);
+    }
+    cv::Mat TSlam::processStereo( cv::Mat &in_image,const cv::Mat &R_image,const ImageParams &ip,uint32_t frameseq_idx){
+        return  reinterpret_cast<System*>(impl)->process(in_image,ip,frameseq_idx,cv::Mat(),R_image);
+    }
+    cv::Mat TSlam::processRGBD( cv::Mat &in_image,const cv::Mat & depth,const ImageParams &ip,uint32_t frameseq_idx){
+        return  reinterpret_cast<System*>(impl)->process(in_image,ip,frameseq_idx,depth);
+    }
+    cv::Mat TSlam::processArray(vector<cv::Mat> &images,  ImageParams &ArrayCamParams ,uint32_t frameseq_idx)
+    {
+        return  reinterpret_cast<System*>(impl)->process(images, ArrayCamParams, frameseq_idx);
+    }
+    void TSlam::resetTracker(){
+        reinterpret_cast<System*>(impl)->resetTracker();
+    }
+    void TSlam::setMode(MODES mode){
+        reinterpret_cast<System*>(impl)->setMode(mode);
+    }
 //void TSlam::resetCurrentPose(){
 //    reinterpret_cast<System*>(impl)->resetCurrentPose();
 //}
-uint32_t TSlam::getLastProcessedFrame()const{
-    return reinterpret_cast<System*>(impl)->getLastProcessedFrame();
-}
-void TSlam::saveToFile(std::string filepath){
-    reinterpret_cast<System*>(impl)->saveToFile(filepath);
-}
-void TSlam::readFromFile(std::string filepath){
-    reinterpret_cast<System*>(impl)->readFromFile(filepath);
-}
-void TSlam::globalOptimization(){
-    reinterpret_cast<System*>(impl)->globalOptimization();
-}
+    uint32_t TSlam::getLastProcessedFrame()const{
+        return reinterpret_cast<System*>(impl)->getLastProcessedFrame();
+    }
+    void TSlam::saveToFile(std::string filepath){
+        reinterpret_cast<System*>(impl)->saveToFile(filepath);
+    }
+    void TSlam::readFromFile(std::string filepath){
+        reinterpret_cast<System*>(impl)->readFromFile(filepath);
+    }
+    void TSlam::globalOptimization(){
+        reinterpret_cast<System*>(impl)->globalOptimization();
+    }
 //waits for all threads to finish
-void TSlam::waitForFinished(){
-    reinterpret_cast<System*>(impl)->waitForFinished();
-}
-uint32_t TSlam::getCurrentKeyFrameIndex(){
-    return reinterpret_cast<System*>(impl)->getCurrentKeyFrameIndex();
-}
+    void TSlam::waitForFinished(){
+        reinterpret_cast<System*>(impl)->waitForFinished();
+    }
+    uint32_t TSlam::getCurrentKeyFrameIndex(){
+        return reinterpret_cast<System*>(impl)->getCurrentKeyFrameIndex();
+    }
 // std::shared_ptr<Map> TSlam::getMap(){
 //     return reinterpret_cast<System*>(impl)->getMap();
 // }
-void TSlam::setDebugLevel(int level){
-    debug::Debug::setLevel(level);
-}
-void TSlam::showTimers(bool v){
-    debug::Debug::showTimer(v);
-}
-void TSlam::updateParams(const Params &p){
-}
-std::string TSlam::getSignatureStr()const{
-    return reinterpret_cast<System*>(impl)->getSignatureStr();
-}
+    void TSlam::setDebugLevel(int level){
+        debug::Debug::setLevel(level);
+    }
+    void TSlam::showTimers(bool v){
+        debug::Debug::showTimer(v);
+    }
+    void TSlam::updateParams(const Params &p){
+    }
+    std::string TSlam::getSignatureStr()const{
+        return reinterpret_cast<System*>(impl)->getSignatureStr();
+    }
 
 /**
 * Things the original UcoSlam doesn't implement
 **/
-void TSlam::clearMap(){
-    
-    map = std::make_shared<tslam::Map>();
-    updateSystem();
-}
-
-void TSlam::setMap(std::string pathToMapFile){
-    // auto newMap = std::shared_ptr<Map> newMap;
-    auto newMap = std::make_shared<tslam::Map>();
-    newMap->readFromFile(pathToMapFile);
-    map = newMap;
-    updateSystem();
-}
-
-void TSlam::setVocabulary(std::string pathToVocFile){
-    this->pathToVoc = pathToVocFile;
-    updateSystem();
-}
-
-void TSlam::setCamParams(std::string pathToCamParamFile){
-    imageParams.readFromXMLFile(pathToCamParamFile);
-}
-
-void TSlam::setInstancing(bool isInstancing){
-    systemParams.isInstancing = isInstancing;
-    auto newMap = map;
-    map = newMap;
-    updateSystem();
-}
-
-bool TSlam::process(cv::Mat frame, cv::Mat &camPose){
-    bool isTracked;
-    camPose = process(frame, imageParams, currentFrameIndex++);
-    if(camPose.empty()){
-        isTracked = false;
-        camPose = cv::Mat::eye(4, 4, CV_32F);
-    } else {
-        isTracked = true;
-        camPose.copyTo(lastTrackedCamPose);
+    void TSlam::clearMap(){
+        toClearMap = true;
     }
-    return isTracked;
-}
 
-void TSlam::updateSystem(){
-    delete (System*)impl;
-    impl = new System;
-    setParams(map, systemParams, pathToVoc);
-}
+    void TSlam::setMap(std::string pathToMapFile, bool updateSystemImmediately){
+        pathToMap = pathToMapFile;
+        toUpdateSystem = true;
+        toResetMap = true;
+        if(updateSystemImmediately)
+            updateSystem();
+    }
+
+    void TSlam::setVocabulary(std::string pathToVocFile){
+        this->pathToVoc = pathToVocFile;
+        toUpdateSystem = true;
+    }
+
+    void TSlam::setCamParams(std::string pathToCamParamFile){
+        imageParams.readFromXMLFile(pathToCamParamFile);
+    }
+
+    void TSlam::setInstancing(bool isInstancing){
+        systemParams.isInstancing = isInstancing;
+        toUpdateSystem = true;
+    }
+
+    bool TSlam::process(cv::Mat frame, cv::Mat &camPose){
+        bool isTracked;
+        camPose = process(frame, imageParams, currentFrameIndex++);
+        if(camPose.empty()){
+            isTracked = false;
+            camPose = cv::Mat::eye(4, 4, CV_32F);
+        } else {
+            isTracked = true;
+            camPose.copyTo(lastTrackedCamPose);
+        }
+        return isTracked;
+    }
+
+    void TSlam::updateSystem(){
+        if(toUpdateSystem | toResetMap | toClearMap){
+            delete (System*)impl;
+            impl = new System;
+
+            if(toResetMap){
+                auto newMap = std::make_shared<tslam::Map>();
+                newMap->readFromFile(pathToMap);
+                map = newMap;
+                toResetMap = false;
+            }
+
+            if(toClearMap){
+                map = std::make_shared<tslam::Map>();
+                toClearMap = false;
+            }
+
+            setParams(map, systemParams, pathToVoc);
+            toUpdateSystem = false;
+        }
+
+
+
+
+    }
 
 }
--- src/tslam.h
+++ src/tslam.h
@@ -25,144 +25,149 @@
 #include "imageparams.h"
 #include "map.h"
 namespace tslam{
-class TSLAM_API TSlam{
-public:
-    TSlam();
-    ~TSlam();
+    class TSLAM_API TSlam{
+    public:
+        TSlam();
+        ~TSlam();
 
-    /**
-     * @brief setParams set the required params before processing images
-     * @param map a pointer to the map being used. It can be an empty map or an already created one
-     * @param params controlling the system behaviuor
-     * @param vocabulary optional path to the dictionary BOW. Without it, relocalization is not yet possible.
-     */
-    void setParams(std::shared_ptr<Map> map, const  tslam::Params &params, const std::string &vocabulary="");
+        /**
+         * @brief setParams set the required params before processing images
+         * @param map a pointer to the map being used. It can be an empty map or an already created one
+         * @param params controlling the system behaviuor
+         * @param vocabulary optional path to the dictionary BOW. Without it, relocalization is not yet possible.
+         */
+        void setParams(std::shared_ptr<Map> map, const  tslam::Params &params, const std::string &vocabulary="");
 
-    //Clear this object setting it to intial state. It will remove all map data
-    void clear();
+        //Clear this object setting it to intial state. It will remove all map data
+        void clear();
 
-    //returns system params
-    static Params &getParams() ;
-
-
-    //Feeds the system with a new image and the parameters of the camera it has been processed with. The final
-    //parameter is the index of the sequence
-    //Returns the camera pose estimated. The pose is the transform moving points from the global reference sytem to the camera reference ssytem
-    cv::Mat process( cv::Mat &in_image,const ImageParams &ip,uint32_t frameseq_idx);
-    cv::Mat processStereo( cv::Mat &left_image,const cv::Mat &right_image,const ImageParams &ip,uint32_t frameseq_idx);
-    cv::Mat processRGBD( cv::Mat &in_image,const cv::Mat & depth,const ImageParams &ip,uint32_t frameseq_idx);
-    cv::Mat processArray(vector<cv::Mat> &images,  ImageParams &ArrayCamParams ,uint32_t frameseq_idx);
-
-    //Reset the current frame pose. Use it to start tracking in a known map
-    void resetTracker();
-
-    //sets the system mode
-    void setMode(MODES mode);
-
-    //sets the system in lost mode
-    //void resetCurrentPose();
-
-    //returns the number of the last processed framed
-    uint32_t getLastProcessedFrame()const;
+        //returns system params
+        static Params &getParams() ;
 
 
-    // Saves the current state of the system to a file so that it can be recovered later. It is only safe if the system is in sequential mode
-    void saveToFile(std::string filepath);
-    //Loads the state of the system from a file
-    void readFromFile(std::string filepath);
+        //Feeds the system with a new image and the parameters of the camera it has been processed with. The final
+        //parameter is the index of the sequence
+        //Returns the camera pose estimated. The pose is the transform moving points from the global reference sytem to the camera reference ssytem
+        cv::Mat process( cv::Mat &in_image,const ImageParams &ip,uint32_t frameseq_idx);
+        cv::Mat processStereo( cv::Mat &left_image,const cv::Mat &right_image,const ImageParams &ip,uint32_t frameseq_idx);
+        cv::Mat processRGBD( cv::Mat &in_image,const cv::Mat & depth,const ImageParams &ip,uint32_t frameseq_idx);
+        cv::Mat processArray(vector<cv::Mat> &images,  ImageParams &ArrayCamParams ,uint32_t frameseq_idx);
+
+        //Reset the current frame pose. Use it to start tracking in a known map
+        void resetTracker();
+
+        //sets the system mode
+        void setMode(MODES mode);
+
+        //sets the system in lost mode
+        //void resetCurrentPose();
+
+        //returns the number of the last processed framed
+        uint32_t getLastProcessedFrame()const;
 
 
-    //only for debugging pourposes perform the global optimization
-    void globalOptimization();
-
-    //waits for all threads to finish
-    void waitForFinished();
-
-
-    //returns the index of the current keyframe
-    uint32_t getCurrentKeyFrameIndex();
+        // Saves the current state of the system to a file so that it can be recovered later. It is only safe if the system is in sequential mode
+        void saveToFile(std::string filepath);
+        //Loads the state of the system from a file
+        void readFromFile(std::string filepath);
 
 
-    //returns a pointer to the map being used
-    inline std::shared_ptr<Map> getMap() { return map; } ;
+        //only for debugging pourposes perform the global optimization
+        void globalOptimization();
+
+        //waits for all threads to finish
+        void waitForFinished();
 
 
-    //returns an string that identifies the system state. It is like a md5 sum the system state
-    std::string getSignatureStr()const;
+        //returns the index of the current keyframe
+        uint32_t getCurrentKeyFrameIndex();
 
 
-    void setDebugLevel(int level);
-    void showTimers(bool v);
-
-    //will update the internal parameters.Not all parameters can be changed
-    void updateParams(const Params &p);
-
-    /**
-    * Things the original UcoSlam doesn't implement
-    **/
-    std::shared_ptr<tslam::Map> map;
-    tslam::Params systemParams;
-    tslam::ImageParams imageParams;
-    
-    /**
-     * @brief Clear the map.
-     */
-    void clearMap();
-    /**
-     * @brief Set the map
-     * @param pathToMap path to the .map file
-     */
-    void setMap(std::string pathToMapFile);
-    /**
-     * @brief Set the vocabulary
-     * @param pathToMap path to the .fbow file
-     */
-    void setVocabulary(std::string pathToVocFile);
-    /**
-     * @brief Set the camera parameter
-     * @param pathToMap Path to the camera parameter file, the structure should be like
-     */
-    void setCamParams(std::string pathToCamParamFile);
-    /**
-     * Set the status of the map
-     * 
-     * Set the status of the map. If "isInstancing" is set to true global optimization will be off and the
-     * new added keyframes will be kept in a fixed number
-     * @param isInstancing true/false
-     */
-    void setInstancing(bool isInstancing);
-    
-    /**
-     * @brief Get the last tracked camera pose (not guarentee to be the last frame)
-     * @return A 4x4 cv::Mat; cv::Mat::eye if not tracked ever.
-     */
-    inline cv::Mat getLastTrackedCamPose() { return lastTrackedCamPose; };
-    /**
-     * @brief get the camera pose of the last processed frame,
-     * @return A 4x4 cv::Mat; cv::eye if last frame was not tracked.
-     */
-    // inline cv::Mat getLastCamPose(){ return lastCamPose; };
-    /**
-     * @brief Process a frame
-     * @param frame Frame to process
-     * @param camPose The reference will be updated to the camera pose of the frame.
-     * @return A boolean indicate if is tracked
-     */
-    bool process(cv::Mat frame, cv::Mat &camPose);
+        //returns a pointer to the map being used
+        inline std::shared_ptr<Map> getMap() { return map; } ;
 
 
-private:
-    void *impl;
-    void updateSystem();
+        //returns an string that identifies the system state. It is like a md5 sum the system state
+        std::string getSignatureStr()const;
 
-    std::string pathToVoc = "";
 
-    // cv::Mat lastCamPose = cv::Mat::eye(4, 4, CV_32F);
-    cv::Mat lastTrackedCamPose = cv::Mat::eye(4, 4, CV_32F);
-    
-    int currentFrameIndex = 0;
+        void setDebugLevel(int level);
+        void showTimers(bool v);
 
-};
+        //will update the internal parameters.Not all parameters can be changed
+        void updateParams(const Params &p);
+
+        /**
+        * Things the original UcoSlam doesn't implement
+        **/
+        std::shared_ptr<tslam::Map> map;
+        tslam::Params systemParams;
+        tslam::ImageParams imageParams;
+
+        /**
+         * @brief Clear the map.
+         */
+        void clearMap();
+        /**
+         * @brief Set the map
+         * @param pathToMap path to the .map file
+         */
+        void setMap(std::string pathToMapFile, bool updateSystemImmediately=false);
+        /**
+         * @brief Set the vocabulary
+         * @param pathToMap path to the .fbow file
+         */
+        void setVocabulary(std::string pathToVocFile);
+        /**
+         * @brief Set the camera parameter
+         * @param pathToMap Path to the camera parameter file, the structure should be like
+         */
+        void setCamParams(std::string pathToCamParamFile);
+        /**
+         * Set the status of the map
+         *
+         * Set the status of the map. If "isInstancing" is set to true global optimization will be off and the
+         * new added keyframes will be kept in a fixed number
+         * @param isInstancing true/false
+         */
+        void setInstancing(bool isInstancing);
+
+        /**
+         * @brief Get the last tracked camera pose (not guarentee to be the last frame)
+         * @return A 4x4 cv::Mat; cv::Mat::eye if not tracked ever.
+         */
+        inline cv::Mat getLastTrackedCamPose() { return lastTrackedCamPose; };
+        /**
+         * @brief get the camera pose of the last processed frame,
+         * @return A 4x4 cv::Mat; cv::eye if last frame was not tracked.
+         */
+        // inline cv::Mat getLastCamPose(){ return lastCamPose; };
+        /**
+         * @brief Process a frame
+         * @param frame Frame to process
+         * @param camPose The reference will be updated to the camera pose of the frame.
+         * @return A boolean indicate if is tracked
+         */
+        bool process(cv::Mat frame, cv::Mat &camPose);
+
+    private:
+        void *impl;
+        void updateSystem();
+
+        std::string pathToVoc = "";
+        std::string pathToMap = "";
+        std::string pathToCamParam = "";
+
+        // cv::Mat lastCamPose = cv::Mat::eye(4, 4, CV_32F);
+        cv::Mat lastTrackedCamPose = cv::Mat::eye(4, 4, CV_32F);
+
+        int currentFrameIndex = 0;
+
+        bool toUpdateSystem = false;
+        bool toClearMap = false, toResetMap = false;
+        bool toResetCamParams = false;
+
+    };
 
 }
 #endif
