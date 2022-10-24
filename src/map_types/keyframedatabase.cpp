d0cf4a1324a905029fdff0ab99d1d57ea69368ce
Andrea Settimi <andreasettimi39@gmail.com>
2022-10-24 19:09:17 +0200

    Update keyframedatabase.cpp

--- src/map_types/keyframedatabase.cpp
+++ src/map_types/keyframedatabase.cpp
@@ -1,21 +1,4 @@
-/**
-* This file is part of  TSLAM
-*
-* Copyright (C) 2018 Rafael Munoz Salinas <rmsalinas at uco dot es> (University of Cordoba)
-*
-* TSLAM is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* TSLAM is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with TSLAM. If not, see <http://wwmap->gnu.org/licenses/>.
-*/
+
 #include "keyframedatabase.h"
 #include <map>
 #include <vector>
