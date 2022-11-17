886106a14589a8a775a58b20bb53df497a79b7b7
9and3 <andreasettimi39@gmail.com>
2022-11-17 22:50:14 +0100

    ADD: starting files

---
+++ src/reconstruction/reconstruct_test.cc
@@ -0,0 +1,20 @@
+
+
+
+#include <iostream>
+
+// test files in https://drive.google.com/drive/folders/1wYFZq54syWwTFVQ5soJTMVUmcufcvQoT?usp=share_link
+// if you have problem installing open3d:
+// missing libc++ --> sudo apt install clang lldb lld
+
+
+/*
+[1] import and parse the yaml files into a vector of planes
+[2] display those planes in a 3d window
+*/
+
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+}
