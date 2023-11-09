9bba14bba34b97f72f870cb8fdfc8389c13e63fd
9and3 <andreasettimi39@gmail.com>
2023-11-09 10:14:05 +0100

    MILESTONE: add correct copyright stamps to src files

--- eval/eval_3D_reconstruct/align.py
+++ eval/eval_3D_reconstruct/align.py
@@ -1,4 +1,4 @@
-/**
+"""
 This file is part of TSLAM and distributed under the terms of the GPLv3.
 
 Copyright (C) 2023 Andrea Settimi <andrea dot settimi at epfl dot ch>, Hong-Bin Yang <hong dash bin dot yang at epfl dot ch> (IBOIS, EPFL)
@@ -26,25 +26,49 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 The views and conclusions contained in the software and documentation are those of the
 authors and should not be interpreted as representing official policies, either expressed
 or implied Andrea Settimi and Hong-Bin Yang.
-*/
-#ifndef __TSLAM_RECONSTRUCTOR_CORE_TYPES_H__
-#define __TSLAM_RECONSTRUCTOR_CORE_TYPES_H__
- 
-#if !defined _CRT_SECURE_NO_DEPRECATE && _MSC_VER > 1300
-#define _CRT_SECURE_NO_DEPRECATE /* to avoid multiple Visual Studio 2005 warnings */
-#endif
+"""
 
-#if (defined WIN32 || defined _WIN32 || defined WINCE)   && defined TSLAM_DSO_EXPORTS
-    #define TSLAM_API __declspec(dllexport)
-    #pragma warning ( disable : 4251 ) //disable warning to templates with dll linkage.
-    #pragma warning ( disable : 4290 ) //disable warning due to exception specifications.
-    #pragma warning ( disable : 4996 ) //disable warning regarding unsafe vsprintf.
-    #pragma warning ( disable : 4244 ) //disable warning convesions with lost of data.
+import numpy as np
+import open3d as o3d
+import copy
 
-#else
-    #define TSLAM_RECONSTRUCTOR_API
-#endif
+def draw_registration_result(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp],
+                                      zoom=0.4459,
+                                      front=[0.9288, -0.2951, -0.2242],
+                                      lookat=[1.6784, 2.0612, 1.4451],
+                                      up=[-0.3402, -0.9189, -0.1996])
 
 
-#define  TSLAM_RECONSTRUCTOR_VERSION "1.0.0"
-#endif
+# Read the point cloud
+source = o3d.io.read_point_cloud("test_2.ply")
+target = o3d.io.read_point_cloud("test_1.ply")
+
+threshold = 10
+trans_init = np.eye(4)
+
+reg_p2p = o3d.pipelines.registration.registration_icp(
+    source, target, threshold, trans_init,
+    o3d.pipelines.registration.TransformationEstimationPointToPoint(),
+    o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=2000)
+    )
+print(reg_p2p)
+print("Transformation is:")
+print(reg_p2p.transformation)
+draw_registration_result(source, target, reg_p2p.transformation)
+
+exit(0)
+
+# Get the oriented bounding box
+obbox = o3d.geometry.AxisAlignedBoundingBox.create_from_points(pcd.points)
+# obbox = obbox.get_minimal_oriented_bounding_box()
+obbox.color = (1, 0, 0)
+print(obbox)
+
+# Visualize the point cloud
+o3d.visualization.draw_geometries([pcd, obbox])
